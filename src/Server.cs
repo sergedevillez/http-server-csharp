d2c0656f42ba3147993c772279c456f47b88b7ae
Serge Devillez <sde@simim.be>
2024-03-11 10:21:09 +0100

    pass 2nd stage

--- src/Server.cs
+++ src/Server.cs
@@ -1,9 +1,25 @@
 using System.Net;
 using System.Net.Sockets;
+using System.Text;
 
-// You can use print statements as follows for debugging, they'll be visible when running tests.
-Console.WriteLine("Logs from your program will appear here!");
-
+//Start server
+Console.WriteLine("Starting server...");
 TcpListener server = new(IPAddress.Any, 4221);
 server.Start();
-server.AcceptSocket(); // wait for client
+Console.WriteLine("Server started on port 4221");
+
+while (true)
+{
+    //Open socket and wait for call from client
+    var socket = server.AcceptSocket();
+
+    //Create response
+    var response = "HTTP/1.1 200 OK\r\n\r\n";
+
+    //Prepare response as byte array
+    var dataBuffer = Encoding.UTF8.GetBytes(response);
+
+    //Send response via saved socket
+    socket.Send(dataBuffer);
+
+}
