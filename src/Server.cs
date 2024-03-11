f8b68cf9f0ece54fbbcea55c7fdc8b99cb6f5fca
Serge Devillez <sde@simim.be>
2024-03-11 11:26:11 +0100

    Modified status code to have 200OK rather than 200IK

--- src/Server.cs
+++ src/Server.cs
@@ -29,10 +29,10 @@ while (true)
     switch (req.Path)
     {
         case "/":
-            await socket.SendAsync("HTTP/1.1 200IK\r\n\r\nYou called the root path");
+            await socket.SendAsync("HTTP/1.1 200OK\r\n\r\nYou called the root path");
             break;
         case "/index.html":
-            await socket.SendAsync("HTTP/1.1 200IK\r\n\r\nYou called the index.html path");
+            await socket.SendAsync("HTTP/1.1 200OK\r\n\r\nYou called the index.html path");
             break;
         default:
             await socket.SendAsync("HTTP/1.1 404NOTFOUND\r\n\r\nPath not found");
